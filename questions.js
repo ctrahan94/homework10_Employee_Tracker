2025bef5b902e2fd3924afc027259603abbb187d
Colby Trahan <ctrahan94@users.noreply.github.com>
2020-10-27 15:37:27 -0600

    Added switch case to questions.js

--- questions.js
+++ questions.js
@@ -1,4 +1,8 @@
 const inquirer = require("inquirer");
+const { connect } = require("./connections");
+const { connection } = require("./db");
+const db = require("./db");
+require("console.table");
 
 const initQuestions = [
   {
@@ -8,17 +12,60 @@ const initQuestions = [
       "View all employees",
       "View all employees by department",
       "View all employees by manager",
-      "Add employee",
+      "Add employee" /* ask series of questions inside of function  (bundle in object -- when call dbcreate pass in object as argument) */,
       "Remove employee",
       "Update employee by role",
       "Update employee by manager",
+      "I am done", // connection.end
     ],
     name: "choices",
   },
 ];
 
-function init(){
-  inquirer.prompt(initQuestions)
-  };
+function init() {
+  inquirer.prompt(initQuestions).then((answers) => {
+    switch (answers.choices) {
+      case "View all employees":
+        viewAllEmployees();
+        break;
 
+      case "View all employees by department":
+        viewAllByDepartment();
+        break;
+
+      case "View all employees by manager":
+        viewAllByManager();
+        break;
+
+      case "Add employee":
+        addEmployee();
+        break;
+
+      case "Remove employee":
+        removeEmployee();
+        break;
+
+      case "Update employee by role":
+        updateEmployeeByRole();
+        break;
+
+      case "Update employee by manager":
+        updateEmployeeByManager();
+        break;
+
+      default:
+        iAmDone();
+        console.log("Done");
+        // connection.done();
+      //come back and finish switch/case
+    }
+  });
+}
+
+async function viewAllEmployees() {
+  var employees = await db.seeAllEmployees();
+  console.table(employees);
   init();
+}
+
+init();
