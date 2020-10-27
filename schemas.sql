479a9bdf0aba66e76ea269346ef8ed689c91b0eb
Colby Trahan <ctrahan94@users.noreply.github.com>
2020-10-27 15:45:17 -0600

    Renamed trackerDB.sql to schemas.sql

---
+++ schemas.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS trackerDB;
+
+CREATE DATABASE trackerDB;
+
+USE trackerDB;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
+
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id)
+-- VALUES ("John", "Doe", 1, 1), ("Mike", "Chan", 2,1), ("Ashley", "Rodriguez", 3, 2);
+
+-- INSERT INTO role (title, salary, department_id)
+-- VALUES ("Sales Lead", 120000, 1), ("Salesperson", 80000, 2), ("Lead Engineer", 150000, 3);
+
+-- INSERT INTO department (name)
+-- VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
